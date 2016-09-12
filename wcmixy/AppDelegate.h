2a7cf73aa95f3f0b06ca3b9d7dd2ed32975c6122
Yi Hua <yhua1@andrew.cmu.edu>
2016-09-11 21:00:40 -0400

    Initial Commit

---
+++ wcmixy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wcmixy
+//
+//  Created by Yi Hua on 9/11/16.
+//  Copyright © 2016 Yi Hua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wcmixy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wcmixy
+//
+//  Created by Yi Hua on 9/11/16.
+//  Copyright © 2016 Yi Hua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
